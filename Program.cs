b15df272bfd9bbf6e6e365b192f0b732f5abff6f
amoreCaro <117117217+amoreCaro@users.noreply.github.com>
2023-07-05 11:37:18 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to the Weather Console App!");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("1. Get Weather");
+        Console.WriteLine("2. Exit");
+        Console.Write("Please enter your choice: ");
+
+        string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    await GetWeather();
+                    break;
+                case "2":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+    }
+}
